e7c2268b95c5b34b0e739ba25e51ca233c889d63
Vinicus Arruda <viniciusarruda189@gmail.com>
2024-02-09 18:53:15 -0300

    feat : criação do service, onde gera a possibilidada de cria, apagar, alterar e deletar serviços.

---
+++ src/main/java/com/desafio/todolist/todolist/service/TodoServive.java
@@ -0,0 +1,37 @@
+package com.desafio.todolist.todolist.service;
+
+import com.desafio.todolist.todolist.entity.Todo;
+import com.desafio.todolist.todolist.repository.TodoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class TodoServive {
+
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> list(){
+       Sort sort =  Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> deleted(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+
+}
